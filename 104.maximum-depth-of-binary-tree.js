69f50d8429723e3b22218eb4b6b5b438ca6d6ab0
seognil LC <seognil@gmail.com>
2019-06-16 15:52:33 +0800

    feat(submit):

---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root === null) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
